93fa2f884015a0036f6ad25bb11a7cb8849e316b
Shreyas Salankimatt <shreyassalankimatt@gmail.com>
2024-05-22 23:45:47 +0530

    Add Field List; Add Select All Button; Add Search Bar;

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import pandas as pd
+import json
 import webbrowser
+
+civil_emp_df = {}
+fields_list_civil_emp = []
 
 app = Flask(
     import_name="Data Fetcher", static_folder="static", static_url_path="/static"
@@ -8,14 +12,32 @@ app = Flask(
 
 
 def read_data():
-    emp_df = pd.read_csv("master-data/Civil-Emp.csv")
-    fields_list_civil_emp = emp_df.iloc[3].tolist()
-    print(fields_list_civil_emp)
+    global fields_list_civil_emp, civil_emp_df
+    civil_emp_df = pd.read_csv("master-data/Civil-Emp.csv", header=3)
+    fields_list_civil_emp = civil_emp_df.iloc[0].tolist()
+
+
+@app.route("/Civil-employee-data", methods=["GET"])
+def civil_emp_data():
+    global fields_list_civil_emp
+    return jsonify({"civil-fields": fields_list_civil_emp})
 
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+
+@app.route("/return-selected-fields", methods=["POST"])
+def return_selected_fields():
+    data = json.loads(request.data.decode("utf-8"))
+    print(data)
+    try:
+        extracted_df = civil_emp_df[data]
+    except KeyError as e:
+        raise ValueError(f"Error extracting columns: {e}")
+    print(extracted_df)
+    return jsonify({"message": "hi!"})
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,122 @@
 const personTypeSelector = document.getElementById("stud-emp");
 const branchSelector = document.getElementById("branch");
 const fieldsList = document.getElementById("fields-list");
+const searchBox = document.getElementById("search-bar");
+const selectAllDiv = document.getElementById("select-all");
+const previewButton = document.getElementById("preview-btn");
+const selectAllButton = selectAllDiv.querySelector("input");
 
-const selectedPersonType = "";
-const selectedBranch = "";
+let selectedPersonType = "";
+let selectedBranch = "";
 
 branchSelector.addEventListener("change", (event) => {
   selectedBranch = event.target.value;
   if (
-    personValue === "none" ||
     selectedBranch === "none" ||
+    selectedPersonType === "none" ||
     selectedPersonType === "" ||
     selectedBranch === ""
   ) {
+    selectAllDiv.classList.add("invisible");
+    fieldsList.innerHTML = `<p id="select-text">--SELECT YOUR BRANCH AND PERSON TYPE--</p>`;
     return;
-    //Optional: Display Message to Select Valid Item
   }
   displayFields(selectedPersonType, selectedBranch);
 });
 
 personTypeSelector.addEventListener("change", (event) => {
-  const personValue = event.target.value;
+  selectedPersonType = event.target.value;
   if (
-    personValue === "none" ||
     selectedBranch === "none" ||
+    selectedPersonType === "none" ||
     selectedPersonType === "" ||
     selectedBranch === ""
   ) {
+    selectAllDiv.classList.add("invisible");
+    fieldsList.innerHTML = `<p id="select-text">--SELECT YOUR BRANCH AND PERSON TYPE--</p>`;
     return;
-    //Optional: Display Message to Select Valid Item
   }
 
   displayFields(selectedPersonType, selectedBranch);
+});
+
+async function fetchFields(selectedPersonType, selectedBranch) {
+  return fetch(`/${selectedBranch}-${selectedPersonType}-data`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const fields = data["civil-fields"];
+      return fields;
+    });
+}
+
+function displayFields(selectedPersonType, selectedBranch) {
+  fetchFields(selectedPersonType, selectedBranch).then((fields) => {
+    fieldsList.innerHTML = "";
+    fields.forEach((field) => {
+      const newField = document.createElement("li");
+      newField.id = field;
+      newField.className = "field";
+      const checkBoxField = document.createElement("input");
+      checkBoxField.type = "checkbox";
+      const checkBoxLabel = document.createElement("label");
+      checkBoxLabel.textContent = field;
+      newField.appendChild(checkBoxField);
+      newField.appendChild(checkBoxLabel);
+      fieldsList.append(newField);
+    });
+  });
+  selectAllDiv.classList.remove("invisible");
+}
+
+searchBox.addEventListener("input", (event) => {
+  if (fieldsList.innerHTML === "") return;
+  const searchTerm = event.target.value.toLowerCase();
+  const checkboxes = document.querySelectorAll("#fields-list li");
+  checkboxes.forEach((checkbox) => {
+    const label = checkbox.querySelector("label");
+    const labelText = label.textContent.toLowerCase();
+
+    if (labelText.includes(searchTerm)) {
+      checkbox.style.display = "block";
+    } else {
+      checkbox.style.display = "none";
+    }
+  });
+});
+
+selectAllButton.addEventListener("click", (event) => {
+  const check = event.target.checked;
+  const checkboxes = document.querySelectorAll("#fields-list li input");
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = check === true ? true : false;
+  });
+});
+
+previewButton.addEventListener("click", () => {
+  const allFields = document.querySelectorAll("#fields-list li");
+  let selectedFields = [];
+  allFields.forEach((field) => {
+    if (field.querySelector("input").checked === true) {
+      selectedFields.push(field.querySelector("label").textContent);
+    }
+  });
+  fetch("/return-selected-fields", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(selectedFields),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
 });
